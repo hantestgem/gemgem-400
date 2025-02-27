7a5684a7852bd49053b07ba055dd448d63fc2480
hantestgem <hantestgem@gmail.com>
2025-02-28 08:52:04 +0900

    Create config.ts

---
+++ src/i18n/config.ts
@@ -0,0 +1,36 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+
+import translationEN from './locales/en/translation.json';
+import translationKO from './locales/ko/translation.json';
+
+const resources = {
+  en: {
+    translation: translationEN,
+  },
+  ko: {
+    translation: translationKO,
+  },
+};
+
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources,
+    fallbackLng: 'ko',
+    supportedLngs: ['en', 'ko'],
+    
+    // URL 기반 언어 감지를 위한 설정
+    detection: {
+      order: ['path', 'navigator'],
+      lookupFromPathIndex: 0,
+    },
+
+    interpolation: {
+      escapeValue: false,
+    },
+  });
+
+export default i18n; 
