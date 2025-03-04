f609de9885b1732c1fe8d8ab28d44b07f19e95c8
hantestgem <hantestgem@gmail.com>
2025-03-04 16:44:05 +0900

    Update config.ts

--- src/i18n/config.ts
+++ src/i18n/config.ts
@@ -5,27 +5,33 @@ import LanguageDetector from 'i18next-browser-languagedetector';
 import translationEN from './locales/en/translation.json';
 import translationKO from './locales/ko/translation.json';
 import translationJP from './locales/jp/translation.json';
-import translationJP from './locales/jp/translation.json';
 
-<re.Match object; span=(381, 544), match="const resources = {\n  en: {\n    translation: tr>
+const resources = {
+  en: {
+    translation: translationEN,
+  },
+  ko: {
+    translation: translationKO,
+
+  jp: {
+    translation: 'translationJP',
+  },
+
 
 i18n
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    resources,
     fallbackLng: 'ko',
-    <re.Match object; span=(582, 615), match="supportedLngs: ['en', 'ko', 'jp']">,
-    
+    supportedLngs: ['en', 'ko', 'jp'],
+
     // URL 기반 언어 감지를 위한 설정
     detection: {
-      order: ['path', 'navigator'],
       lookupFromPathIndex: 0,
     },
 
     interpolation: {
       escapeValue: false,
-    },
   });
 
-export default i18n; 
+export default i18n;
